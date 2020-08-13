0aea9df3daf2cf7f8c3d2b918f57c4adc8738a09
Patrick Song <32438365+patricksongzy@users.noreply.github.com>
2020-08-12 22:59:47 -0400

    Implement basic STFT of microphone data
    Current implementation does not support overlaps.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,65 @@
+use portaudio as pa;
+use std::sync::mpsc::*;
+
 use fft::fft::*;
-use complex::complex::*;
+
+const WINDOW_SIZE: usize = 256;
 
 fn main() {
-    let mut signal = create_signal(0.5, 128);
-    rfft_r2(&mut signal);
-
-    println!("{:?}", &signal[..64]);
-    println!("{:?}", &signal[65..]);
+    match run() {
+        Ok(_) => {}
+        e => {
+            eprintln!("Failed to run PortAudio with: {:?}", e)
+        }
+    }
 }
 
-fn create_signal(frequency: Float, signal_length: usize) -> Vec<Float> {
-    let coefficient: Float = 2.0 * std::f64::consts::PI * frequency;
-    (0..signal_length).map(|x| (coefficient * x as Float).sin()).collect()
+fn run() -> Result<(), pa::Error> {
+    let pa = pa::PortAudio::new()?;
+    let mic_index = pa.default_input_device()?;
+    let mic = pa.device_info(mic_index)?;
+
+    let input_params = pa::StreamParameters::<f32>::new(mic_index, 1, true, mic.default_low_input_latency);
+
+    let input_settings = pa::InputStreamSettings::new(input_params, mic.default_sample_rate, WINDOW_SIZE as u32);
+
+    let (sender, receiver) = channel();
+
+    let mut circular_buffer = vec![0.0_f32; 4 * WINDOW_SIZE];
+    let mut read = 0;
+    let mut write = 0;
+
+    let rotating_vectors = create_rotating_vectors(WINDOW_SIZE);
+
+    // TODO ensure signal increments by time frame
+    let callback = move |pa::InputStreamCallbackArgs {buffer, ..}| {
+        circular_buffer.splice(write..write + WINDOW_SIZE, buffer.iter().cloned());
+        write = (write + WINDOW_SIZE) % circular_buffer.len();
+
+        // TODO overlap is required due to windowing function
+        let mut output_signal = circular_buffer[read..read + WINDOW_SIZE].iter().map(|x| *x as f64).collect();
+        iterative_hann(&mut output_signal);
+        iterative_rfft(&mut output_signal, &rotating_vectors);
+
+        read = (read + WINDOW_SIZE) % circular_buffer.len();
+
+        match sender.send(output_signal) {
+            Ok(_) => pa::Continue,
+            Err(_) => pa::Complete,
+        }
+    };
+
+    let mut stream = pa.open_non_blocking_stream(input_settings, callback)?;
+    stream.start()?;
+
+    while let true = stream.is_active()? {
+        while let Ok(buffer) = receiver.try_recv() {
+            println!("{:?}", buffer);
+        }
+    }
+
+    stream.stop()?;
+
+    Ok(())
 }
+
